18694c303afa11787cff88f4fb691b578450f186
jlbuild <juliabuildbot@gmail.com>
2026-02-09 09:52:00 +0000

    libpolymake_julia_jll build 0.14.5+0

--- src/libpolymake_julia_jll.jl
+++ src/libpolymake_julia_jll.jl
@@ -5,5 +5,5 @@ using Base: UUID
 import JLLWrappers
 
 JLLWrappers.@generate_main_file_header("libpolymake_julia")
-JLLWrappers.@generate_main_file("libpolymake_julia", UUID("4d8266f6-2b3b-57e3-ad7a-d431eaaac945"))
+JLLWrappers.@generate_main_file("libpolymake_julia", Base.UUID("4d8266f6-2b3b-57e3-ad7a-d431eaaac945"))
 end  # module libpolymake_julia_jll
